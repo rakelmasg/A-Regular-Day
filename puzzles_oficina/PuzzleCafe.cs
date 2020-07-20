d27cefaaeb949f219cad1562b7304c47f66729ab
Raquel Mas <rakelmasg@hotmail.com>
2020-07-20 20:59:17 +0200

    Update PuzzleCafe.cs

--- puzzles_oficina/PuzzleCafe.cs
+++ puzzles_oficina/PuzzleCafe.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections;
+/*
+	Fecha creación: 24/12/2017 
+	Autor: Raquel Mas
+	
+*/
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
