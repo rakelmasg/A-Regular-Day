f50c707d3594138b0a98c27d94824687ec65560e
Raquel Mas <rakelmasg@hotmail.com>
2020-07-20 21:09:36 +0200

    Update CogerEngranaje.cs

--- puzzles_isla/CogerEngranaje.cs
+++ puzzles_isla/CogerEngranaje.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections;
+/*
+	Fecha creación: 26/12/2017 
+	Autor: Raquel Mas
+	
+*/
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
