02e560dcd7bf5bdf778308f975afb28d7d7eeaab
Raquel Mas <rakelmasg@hotmail.com>
2020-07-20 20:45:45 +0200

    Update Puzzle_columnas.cs

--- puzzles_piramide/Puzzle_columnas.cs
+++ puzzles_piramide/Puzzle_columnas.cs
@@ -1,4 +1,10 @@
-﻿using System.Collections;
+
+/*
+	Fecha creación: 21/12/2017 
+	Autor: Raquel Mas
+	
+*/
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
